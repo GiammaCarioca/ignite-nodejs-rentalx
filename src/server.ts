48be8debb4de96614c72387358dcd0ffdd187bdc
Thiago Giammattey <giammattey@gmail.com>
2021-03-19 11:49:35 -0300

    add Router and create new category

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
+
 app.use(express.json());
+app.use(categoriesRoutes);
 
 app.get("/", (req, res) => res.json({ message: "Hello World" }));
 
